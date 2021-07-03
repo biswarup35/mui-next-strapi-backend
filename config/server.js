44bc2e17038400e9005d13fa7fa5218f40a4bb09
Biswarup35 <biswarup35@gmail.com>
2021-07-03 23:06:44 +0530

    Project initialized with initial config

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2f95abc77e259aec889ac2974950607b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7f2e0976-0776-446e-a880-a35f2ca1c232'
+};
