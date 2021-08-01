7dab829ecb26f78c74c86fb3fa256fd29c4569f4
Biswarup35 <biswarup35@gmail.com>
2021-08-01 20:53:06 +0530

    Fix port

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 3000),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '2f95abc77e259aec889ac2974950607b'),
+      secret: env("ADMIN_JWT_SECRET", "2f95abc77e259aec889ac2974950607b"),
     },
   },
 });
