c202967ff71a09e719654d6b05fbd4b9fcc6a63f
Biswarup Bouri <50357727+biswarup35@users.noreply.github.com>
2021-08-01 22:05:55 +0530

    port fix

--- config/server.js
+++ config/server.js
@@ -1,4 +1,17 @@
-module.exports = ({ env }) => ({
+module.exports = ({ env }) => {
+  if(env("NODE_ENV") === "production"){
+   return { 
+    host: env("HOST", "0.0.0.0"),
+    port: env("PORT"),
+    production: true,
+    admin: {
+      auth: {
+        secret: env("ADMIN_JWT_SECRET", "2f95abc77e259aec889ac2974950607b"),
+        },
+    },
+    }
+  }
+return {
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT"),
   admin: {
@@ -6,4 +19,5 @@ module.exports = ({ env }) => ({
       secret: env("ADMIN_JWT_SECRET", "2f95abc77e259aec889ac2974950607b"),
     },
   },
-});
+}
+};
