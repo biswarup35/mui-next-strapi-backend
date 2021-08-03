56a383d63e05da3d5a781a91a0805bf8959cf8bd
Biswarup35 <biswarup35@gmail.com>
2021-08-03 12:55:43 +0530

    Cloudinary config added

---
+++ config/plugins.js
@@ -0,0 +1,13 @@
+module.export = ({ env }) => {
+  if (env("NODE_ENV") === "production") {
+    return {
+      upload: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+    };
+  }
+  return {};
+};
