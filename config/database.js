7e3946f57f76ff1a4106f67ca449c608959c8b3a
Biswarup35 <biswarup35@gmail.com>
2022-02-11 12:22:15 +0530

    [update] config

--- config/database.js
+++ config/database.js
@@ -1,26 +1,26 @@
 module.exports = ({ env }) => {
-  if (env("NODE_ENV") === "production") {
-    return {
-      defaultConnection: "default",
-      connections: {
-        default: {
-          connector: "mongoose",
-          settings: {
-            host: env("DATABASE_HOST"),
-            srv: env.bool("DATABASE_SRV", true),
-            port: env.int("DATABASE_PORT", 27017),
-            database: env("DATABASE_NAME"),
-            username: env("DATABASE_USERNAME"),
-            password: env("DATABASE_PASSWORD"),
-          },
-          options: {
-            authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
-            ssl: env.bool("DATABASE_SSL", true),
-          },
-        },
-      },
-    };
-  }
+  // if (env("NODE_ENV") === "production") {
+  //   return {
+  //     defaultConnection: "default",
+  //     connections: {
+  //       default: {
+  //         connector: "mongoose",
+  //         settings: {
+  //           host: env("DATABASE_HOST"),
+  //           srv: env.bool("DATABASE_SRV", true),
+  //           port: env.int("DATABASE_PORT", 27017),
+  //           database: env("DATABASE_NAME"),
+  //           username: env("DATABASE_USERNAME"),
+  //           password: env("DATABASE_PASSWORD"),
+  //         },
+  //         options: {
+  //           authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+  //           ssl: env.bool("DATABASE_SSL", true),
+  //         },
+  //       },
+  //     },
+  //   };
+  // }
   return {
     defaultConnection: "default",
     connections: {
